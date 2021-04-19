b96c06a53d666f2c4b2acfc5c17e75f2dbca9890
joshuarg007 <42895544+joshuarg007@users.noreply.github.com>
2021-04-19 12:41:44 -0600

    added spaces

--- SimpleWebServer/src/edu/nmsu/cs/webserver/WebServer.java
+++ SimpleWebServer/src/edu/nmsu/cs/webserver/WebServer.java
@@ -17,49 +17,38 @@ package edu.nmsu.cs.webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class WebServer
-{
-	private ServerSocket	socket;
+public class WebServer {
+	private ServerSocket socket;
 
-	private boolean				running;
+	private boolean running;
 
 	/**
 	 * Constructor
 	 **/
-	private WebServer()
-	{
+	private WebServer() {
 		running = false;
 	}
 
 	/**
-	 * Web server starting point. This method does not return until the server is finished, so perhaps
-	 * it should be named "runServer" or something like that.
+	 * Web server starting point. This method does not return until the server is
+	 * finished, so perhaps it should be named "runServer" or something like that.
 	 * 
-	 * @param port
-	 *          is the TCP port number to accept connections on
+	 * @param port is the TCP port number to accept connections on
 	 **/
-	private boolean start(int port)
-	{
+	private boolean start(int port) {
 		Socket workerSocket;
 		WebWorker worker;
-		try
-		{
+		try {
 			socket = new ServerSocket(port);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("Error binding to port " + port + ": " + e);
 			return false;
 		}
-		while (true)
-		{
-			try
-			{
+		while (true) {
+			try {
 				// wait and listen for new client connection
 				workerSocket = socket.accept();
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				System.err.println("No longer accepting: " + e);
 				break;
 			}
@@ -73,38 +62,29 @@ public class WebServer
 	/**
 	 * Does not do anything, since start() never returns.
 	 **/
-	private boolean stop()
-	{
+	private boolean stop() {
 		return true;
 	}
 
 	/**
-	 * Application main: process command line and start web server; default port number is 8080 if not
-	 * given on command line.
+	 * Application main: process command line and start web server; default port
+	 * number is 8080 if not given on command line.
 	 **/
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		int port = 8080;
-		if (args.length > 1)
-		{
+		if (args.length > 1) {
 			System.err.println("Usage: java Webserver <portNumber>");
 			return;
-		}
-		else if (args.length == 1)
-		{
-			try
-			{
+		} else if (args.length == 1) {
+			try {
 				port = Integer.parseInt(args[0]);
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				System.err.println("Argument must be an int (" + e + ")");
 				return;
 			}
 		}
 		WebServer server = new WebServer();
-		if (!server.start(port))
-		{
+		if (!server.start(port)) {
 			System.err.println("Execution failed!");
 		}
 	} // end main
